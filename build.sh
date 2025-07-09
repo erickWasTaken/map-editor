0d66444a4150362cae90300900b0d43f503fca9d
erickWasTaken <gabriex86@gmail.com>
2025-07-10 05:41:35 +0900

    First point

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-defines="-DENGINE"
 warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations -Wno-switch"
-includes="-Ithird_party -Ithird_party/Include"
-
-timestamp=$(date +%s)
 
 if [[ "$(uname)" == "Linux" ]]; then
     echo "Running on Linux"
     libs="-lGL -lglut"
     outputFile=map-editor
-
-    # rm -f game_*
-    # clang++ -g "src/game.cpp" -shared -fPIC -o game_$timestamp.so $warnings $defines
-    # mv game_$timestamp.so game.so
 fi
 
-clang $includes -g src/main.c -o$outputFile $libs $warnings $defines
+clang $includes -g src/main.c -o$outputFile $libs $warnings
--- src/main.c
+++ src/main.c
@@ -1,16 +1,53 @@
 #include <GL/glut.h>
+#include <GL/gl.h>
+#include <stdio.h>
+
+#define res 1
+#define SW 160*res
+#define SH 120*res
+#define pixelScale 4/res
+#define GLSW SW*pixelScale
+#define GLSH SH*pixelScale
+
+typedef struct{
+    int mouseX, mouseY;
+    int wallTex, wallU, wallV;
+    int surfTex, surfScale;
+    int bottom, top;
+    int scale;
+    int selS, selW;
+}grid; grid G;
+
+void init(){
+    G.scale = 4;
+    G.selS = 0; G.selW = 0;
+    G.bottom = 0; G.top = 40;
+    G.surfTex = 1; G.surfScale = 4;
+    G.wallTex = 0; G.wallU = 1; G.wallV = 1;
+}
+
+void drawPixel(int x, int y, int r, int g, int b){
+    glColor3ub(r, g, b);
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+}
 
 void display(){
+    drawPixel(0, 0, 255, 255, 255);
     glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 int main(int argc, char* argv[]){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);    // Display with double RGBA buffering
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);            // Display with double RGBA buffering
     glutInitWindowPosition(0, 0);
+    glutInitWindowSize(GLSW, GLSH);
     glutCreateWindow("Map Editor");
-    glPointSize(5);                                 // Pixel size
-    glViewport(0, 0, 1280, 720);
+    glPointSize(pixelScale);                                 // Pixel size
+    glViewport(0, 0, GLSW, GLSH);
+    init();
 
     glutDisplayFunc(display);
 
