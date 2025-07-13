bc62a94cdfd1c4580bec5b679118be52fd9078d5
erickWasTaken <gabriex86@gmail.com>
2025-07-13 11:46:27 +0900

    Line rendering bug fix

--- src/main.c
+++ src/main.c
@@ -39,7 +39,7 @@ void drawPixel(int x, int y, int r, int g, int b){
     glEnd();
 }
 
-void drawLine(int x1, int y1, int x2, int y2, int r, int g, int b){
+void drawLine(float x1, float y1, float x2, float y2, int r, int g, int b){
     float x = x2 - x1;
     float y = y2 - y1;
     float max = fabs(x);
